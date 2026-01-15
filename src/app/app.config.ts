6ad166b0fa5f465374be5193ba46e0600dfe697e
tampv <phamtampt2k1@gmail.com>
2026-01-15 23:40:10 +0700

    add ngzoro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideNzI18n, vi_VN } from 'ng-zorro-antd/i18n';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),provideAnimations(),provideNzI18n(vi_VN)]
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NzButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+    
 }
